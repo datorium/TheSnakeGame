685bf375e3e130ad24541565faaba1ae164acdf2
Datorium <hello@datorium.lv>
2020-07-11 16:43:48 +0300

    version 16:43

--- Snake.cs
+++ Snake.cs
@@ -19,13 +19,20 @@ namespace TheSnakeGame
 
         private void InitializeSnake()
         {
+            this.AddPixel(300, 300);
+            this.AddPixel(300, 320);
+            this.AddPixel(300, 340);
+        }
+
+        private void AddPixel(int left, int top)
+        {
             PictureBox snakePixel;
             snakePixel = new PictureBox();
             snakePixel.Height = 20;
             snakePixel.Width = 20;
             snakePixel.BackColor = Color.Orange;
-            snakePixel.Left = 300;
-            snakePixel.Top = 300;            
+            snakePixel.Left = left;
+            snakePixel.Top = top;
             snakePixels.Add(snakePixel);
         }
 
