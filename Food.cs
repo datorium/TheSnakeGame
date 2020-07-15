be8026b2d62b68f668ee2cdf91f152e6921d3f62
Datorium <hello@datorium.lv>
2020-07-15 17:12:24 +0300

    version 17:12

---
+++ Food.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace TheSnakeGame
+{
+    class Food : PictureBox
+    {
+        public Food()
+        {
+            InitializeFood();
+        }
+
+        private void InitializeFood()
+        {
+            this.Width = 20;
+            this.Height = 20;
+            this.BackColor = Color.Green;
+        }
+
+    }
+
+    
+}
