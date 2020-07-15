cc51556d911c5bec1225cd74d4ad16f53b3f06f4
Datorium <hello@datorium.lv>
2020-07-15 15:57:25 +0300

    version 15:57

--- Game.cs
+++ Game.cs
@@ -12,9 +12,6 @@ namespace TheSnakeGame
 {
     public partial class Game : Form
     {
-        int horVelocity = 0;
-        int verVelocity = 0;
-        int step = 20;        
         Area area = new Area();
         Snake snake = new Snake();
         Timer mainTimer = new Timer();
@@ -26,6 +23,9 @@ namespace TheSnakeGame
             InitializeTimer();
         }
 
+
+
+
         private void InitializeTimer()
         {
             mainTimer.Interval = 500;
@@ -35,7 +35,7 @@ namespace TheSnakeGame
 
         private void MainTimer_Tick(object sender, EventArgs e)
         {
-
+            snake.Move();
         }
 
         private void InitializeGame()
@@ -50,7 +50,36 @@ namespace TheSnakeGame
 
             //adding snake body
             snake.Render(this);
+
+            //adding controls
+            this.KeyDown += new KeyEventHandler(Game_KeyPress);
         }
 
+        private void Game_KeyPress(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    snake.HorVelocity = 1;
+                    snake.VerVelocity = 0;
+                    break;
+                case Keys.Down:
+                    snake.HorVelocity = 0;
+                    snake.VerVelocity = 1;
+                    break;
+                case Keys.Left:
+                    snake.HorVelocity = -1;
+                    snake.VerVelocity = 0;
+                    break;
+                case Keys.Up:
+                    snake.HorVelocity = 0;
+                    snake.VerVelocity = -1;
+                    break;
+                case Keys.B:
+                    snake.AddPixel(snake.snakePixels[snake.snakePixels.Count - 1].Left, snake.snakePixels[snake.snakePixels.Count - 1].Top);
+                    snake.Render(this, -2);
+                    break;
+            }
+        }
     }
 }
--- Snake.cs
+++ Snake.cs
@@ -10,6 +10,10 @@ namespace TheSnakeGame
 {
     class Snake
     {
+        public int Step { get; set; } = 20;
+        public int HorVelocity { get; set; } = 0;
+        public int VerVelocity { get; set; } = 0;
+
         public List<PictureBox> snakePixels = new List<PictureBox>();
 
         public Snake()
@@ -24,7 +28,7 @@ namespace TheSnakeGame
             this.AddPixel(300, 340);
         }
 
-        private void AddPixel(int left, int top)
+        public void AddPixel(int left, int top)
         {
             PictureBox snakePixel;
             snakePixel = new PictureBox();
@@ -36,12 +40,48 @@ namespace TheSnakeGame
             snakePixels.Add(snakePixel);
         }
 
-        public void Render(Form form)
+        public void Move()
         {
-            foreach(var sp in snakePixels)
+            if(this.VerVelocity == 0 && this.HorVelocity == 0)
             {
-                form.Controls.Add(sp);
-                sp.BringToFront();
+                return;
+            }
+
+            for(int i = this.snakePixels.Count-1; i > 0; i--)
+            {
+                snakePixels[i].Location = snakePixels[i - 1].Location;
+            }
+
+            this.snakePixels[0].Left += HorVelocity * Step;
+            this.snakePixels[0].Top += VerVelocity * Step;
+        }
+
+        public void Render(Form form, int index = -1)
+        {
+            switch (index)
+            {
+                case -1: //render all
+                    foreach (var sp in snakePixels)
+                    {
+                        form.Controls.Add(sp);
+                        sp.BringToFront();
+                    }
+                    break;
+                case -2: //render the last                    
+                    form.Controls.Add(snakePixels[snakePixels.Count - 1]);
+                    snakePixels[snakePixels.Count - 1].BringToFront();
+                    break;
+                default: //render pixel with specific index
+                    try
+                    {
+                        form.Controls.Add(snakePixels[index]);
+                        snakePixels[index].BringToFront();
+                    }
+                    catch
+                    {
+
+                    }
+                    break;
             }
         }
     }
